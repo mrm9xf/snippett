5c44cad1ab9e08e8a733cf99b202cabfac82bfb8
mrm9xf <mrm9xf@gmail.com>
2017-01-11 00:26:29 -0500

    first successful api calls to create collections and snips via python console and requests package

--- web_service.py
+++ web_service.py
@@ -1,5 +1,5 @@
-import utilities
-from bottle import get, route, run, template, static_file
+from utilities import utilities
+from bottle import get, route, run, request, response, template, static_file
 
 # Static Routes
 @get("/css/<filepath:re:.*\.css>")
@@ -14,9 +14,57 @@ def img(filepath):
 def js(filepath):
     return static_file(filepath, root="js")
 
+@get("/html/<filepath:re:.*\.html")
+def html(filepath):
+    return static_file(filepath, root="html")
+
 @route('/home')
 def home():
-    return static_file('home.html', root="html")
+    return html('home.html', method='GET')
+
+@route('/create_collection', method='POST')
+def create_collection():
+    #build connection
+    connection = utilities.build_connection()
+
+    #get passed args
+    response.content_type = 'application/json'
+    collection_name       = str(request.json.get('collectionName', ''))
+    author                = str(request.json.get('author', ''))
+
+    #dummy logic, will progress with utilities to check stuff
+    if len(collection_name) and len(author):
+        collection_id = utilities.create_collection(collection_name, author, connection)
+        msg = "collection created"
+    else:
+        msg = 'uh oh, no collection'
+
+    #close connection
+    connection.close()
+
+    return "<html><body><p>{0}</p></body></html>".format(msg)
+
+@route('/create_snip', method='POST')
+def create_collection():
+    #build connection
+    connection = utilities.build_connection()
+
+    #get passed args
+    response.content_type = 'application/json'
+    snip_text             = str(request.json.get('snipText', ''))
+    collection_id         = int(request.json.get('collectionId', 0))
+
+    #dummy logic, will progress with utilities to check stuff
+    if len(snip_text) and collection_id:
+        snip_id = utilities.write_snip(snip_text, collection_id, connection)
+        msg = "snip written"
+    else:
+        msg = 'uh oh, no snip'
+
+    #close connection
+    connection.close()
+
+    return "<html><body><p>{0}</p></body></html>".format(msg)
 
 @route('/test')
 def test():
